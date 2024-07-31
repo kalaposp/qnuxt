81b07e4c562f52ac8874182fca654456d3dafc8b
kalaposp <peter.kalapos@audiohungary.hu>
2024-07-31 15:17:10 +0200

    commit-13 240731

---
+++ build-assets/CVhZF_0K.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c,a as n}from"./B7pNvW4p.js";const t={},s=n("p",null,"Technology",-1),_=[s];function a(r,l){return c(),o("div",null,_)}const i=e(t,[["render",a]]);export{i as default};
