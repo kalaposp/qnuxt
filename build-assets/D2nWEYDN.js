2209250ae19249c018aaa169aa4709ace6727d58
kalaposp <peter.kalapos@audiohungary.hu>
2024-08-13 13:11:28 +0200

    commit-55 240813

--- build-assets/D2nWEYDN.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./VrSs2m_o.js";import{c as t,o,a as r}from"./CkyTbtqQ.js";const a=r("p",null,"Our Story",-1),s=[a],i={__name:"index",setup(n){return e({title:"Qualiton | Our Story"}),(c,_)=>(o(),t("div",null,s))}};export{i as default};
---
+++ build-assets/DgNs38IJ.js
@@ -0,0 +1,1 @@
+import{u as e}from"./CeFiNHvW.js";import{c as t,o,a}from"./CWsOSox9.js";const s=a("p",null,"Support",-1),r=[s],l={__name:"index",setup(n){return e({title:"Qualiton | Support"}),(c,p)=>(o(),t("div",null,r))}};export{l as default};
