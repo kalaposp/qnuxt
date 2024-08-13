d99e8e7d8bd0ad04aaa98b402c45b6421128db37
kalaposp <peter.kalapos@audiohungary.hu>
2024-08-13 12:01:47 +0200

    commit-54 240813

--- build-assets/BjSxDKUE.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./DOSxYIIR.js";import{_ as t}from"./CBHlQlkH.js";import{c as a,b as r,w as i,o,d as s}from"./Dh412n3a.js";import"./gGXVIN7G.js";import"./DsvKCfiI.js";const f={__name:"index",setup(c){return e({title:"Qualiton | Reviews"}),(m,n)=>(o(),a("div",null,[r(t,{id:"reviews-bar",path:"./reviews"},{heading:i(()=>[s(" Reviews ")]),_:1})]))}};export{f as default};
---
+++ build-assets/CX2bcZBg.js
@@ -0,0 +1,1 @@
+import{u as e}from"./VrSs2m_o.js";import{c as t,o,a}from"./CkyTbtqQ.js";const s=a("p",null,"X200",-1),n=[s],p={__name:"index",setup(r){return e({title:"Qualiton | X200 Integrated Amplifier"}),(c,_)=>(o(),t("div",null,n))}};export{p as default};
