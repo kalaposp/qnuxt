000ee17aedc10eef88d049893c17f374c376325c
kalaposp <peter.kalapos@audiohungary.hu>
2024-08-15 10:50:31 +0200

    commit-66 240815

---
+++ build-assets/D4UAUnEm.js
@@ -0,0 +1,1 @@
+import{u as t}from"./w-GUY6FT.js";import{c as e,o,a}from"./C8z7oJNQ.js";const s=a("p",null,"Contact Us",-1),n=[s],u={__name:"index",setup(c){return t({title:"Qualiton | Contact Us"}),(r,_)=>(o(),e("div",null,n))}};export{u as default};
---
+++ build-assets/DrtsJlyD.js
@@ -0,0 +1,1 @@
+import{u as e}from"./w-GUY6FT.js";import{c as t,o,a}from"./C8z7oJNQ.js";const s=a("p",{class:"max-w-7xl mx-auto"},"Support",-1),r=[s],l={__name:"index",setup(c){return e({title:"Qualiton | Support"}),(n,p)=>(o(),t("div",null,r))}};export{l as default};
