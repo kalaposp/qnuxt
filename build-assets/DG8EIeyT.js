41a18dc234ac8c88c8828c59b73043ed36d919af
kalaposp <peter.kalapos@audiohungary.hu>
2024-08-26 09:19:01 +0200

    commit-90 240826

--- build-assets/DG8EIeyT.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./WWmoxQET.js";import{c as t,o,a}from"./Dj28x-1R.js";const s=a("p",null,"X200",-1),n=[s],p={__name:"index",setup(r){return e({title:"Qualiton | X200 Integrated Amplifier"}),(c,_)=>(o(),t("div",null,n))}};export{p as default};
--- build-assets/GjBsrwhu.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./WWmoxQET.js";import{c as o,o as t,a}from"./Dj28x-1R.js";const n=a("p",null,"Technology",-1),s=[n],u={__name:"index",setup(c){return e({title:"Qualiton | Technology"}),(l,r)=>(t(),o("div",null,s))}};export{u as default};
