7de913583b6f0927807bbae49268f81ca605b356
kalaposp <peter.kalapos@audiohungary.hu>
2024-09-16 13:17:58 +0200

    commit-113 240916

---
+++ build-assets/CNo9YQUV.js
@@ -0,0 +1,1 @@
+import{u as e}from"./Cs7XzerR.js";import{c as t,o,a}from"./BdcLOmTg.js";const s=a("p",null,"X200",-1),n=[s],p={__name:"index",setup(r){return e({title:"Qualiton | X200 Integrated Amplifier"}),(c,_)=>(o(),t("div",null,n))}};export{p as default};
