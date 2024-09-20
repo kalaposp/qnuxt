f0109e2ff166d550c827ed3b3910f757569d6f06
kalaposp <peter.kalapos@audiohungary.hu>
2024-09-20 09:04:29 +0200

    commit-129 240920

---
+++ build-assets/BnNGP4Wb.js
@@ -0,0 +1,1 @@
+import{u as e}from"./CAFY_NVs.js";import{c as o,o as t,a}from"./DKpjigCs.js";const n=a("p",null,"Technology",-1),s=[n],u={__name:"index",setup(c){return e({title:"Qualiton | Technology"}),(l,r)=>(t(),o("div",null,s))}};export{u as default};
