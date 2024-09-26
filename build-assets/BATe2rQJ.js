edf64d1499e90d021e1a5023ee6f34df40825e58
kalaposp <peter.kalapos@audiohungary.hu>
2024-09-26 12:41:19 +0200

    commit-134 240926

---
+++ build-assets/BATe2rQJ.js
@@ -0,0 +1,1 @@
+import{u as e}from"./BnUXaIBq.js";import{c as t,o,a}from"./Dvjq6e_F.js";const s=a("p",null,"P200",-1),r=[s],p={__name:"index",setup(n){return e({title:"Qualiton | P200 Power Amplifier"}),(c,_)=>(o(),t("div",null,r))}};export{p as default};
--- build-assets/G7yM2oVh.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./CyXR5ivw.js";import{c as t,o,a}from"./HFHHSG4M.js";const s=a("p",null,"X200",-1),n=[s],p={__name:"index",setup(r){return e({title:"Qualiton | X200 Integrated Amplifier"}),(c,_)=>(o(),t("div",null,n))}};export{p as default};
