da3f25140b824969fa619ec69fb04e2740516c52
kalaposp <peter.kalapos@audiohungary.hu>
2024-10-02 09:17:55 +0200

    commit-148 241002

--- build-assets/TV8szI8m.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./DeMrA5QK.js";import{u as t,c as o,b as n,o as s}from"./C1iOBDed.js";const i={__name:"index",setup(c){return t({title:"Qualiton | Dummy Page"}),(m,r)=>{const e=a;return s(),o("div",null,[n(e,{src:"/img/manuals/300b.jpg"})])}}};export{i as default};
