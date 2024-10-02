e41e7b98133521b3261a9bd379306b5d2953adf8
kalaposp <peter.kalapos@audiohungary.hu>
2024-10-02 10:26:39 +0200

    commit-149 241002

--- build-assets/DFSP7JrJ.js
+++
@@ -1,1 +0,0 @@
-import{u as e,c as o,o as t,a}from"./Dp0hsxMO.js";const n=a("p",null,"Technology",-1),s=[n],u={__name:"index",setup(c){return e({title:"Qualiton | Technology"}),(l,_)=>(t(),o("div",null,s))}};export{u as default};
--- build-assets/lOC1Do_p.js
+++
@@ -1,1 +0,0 @@
-import{u as e,c as t,o as a,a as o}from"./Dp0hsxMO.js";const s=o("p",null,"Legacy Products",-1),c=[s],l={__name:"index",setup(n){return e({title:"Qualiton | Legacy Products"}),(r,_)=>(a(),t("div",null,c))}};export{l as default};
