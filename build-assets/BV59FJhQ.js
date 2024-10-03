9bf9b9db5ecb9f8ad0415da06ba84937a240a471
kalaposp <peter.kalapos@audiohungary.hu>
2024-10-03 20:43:17 +0200

    commit-154 241003

---
+++ build-assets/BV59FJhQ.js
@@ -0,0 +1,1 @@
+import{u as e,c as t,o,a}from"./C40V044G.js";const s=a("p",null,"P200",-1),n=[s],i={__name:"index",setup(c){return e({title:"Qualiton | P200 Power Amplifier"}),(r,_)=>(o(),t("div",null,n))}};export{i as default};
