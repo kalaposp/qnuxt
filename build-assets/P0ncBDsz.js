07130439a86a6e943f3f5a54f7558f502af7c9fd
kalaposp <peter.kalapos@audiohungary.hu>
2024-10-04 07:44:25 +0200

    commit-155 241004

--- build-assets/P0ncBDsz.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./xjmJuqjA.js";import{u as s,c as t,b as e,o as r}from"./C40V044G.js";const g={__name:"index",setup(c){return s({title:"Qualiton | Dummy Page"}),(n,o)=>{const a=m;return r(),t("div",null,[e(a,{src:"/img/manuals/300b.jpg"}),e(a,{src:"/img/manuals/300b-gray-50.jpg"}),e(a,{src:"/img/dummy/bg-gray-50-16x9.jpg"})])}}};export{g as default};
