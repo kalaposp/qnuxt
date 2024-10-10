2884cf0b8512aacee5699214c7ce28a4df82a01c
kalaposp <peter.kalapos@audiohungary.hu>
2024-10-10 08:53:11 +0200

    commit-169 241010

--- build-assets/C7H8ihGh.js
+++
@@ -1,1 +0,0 @@
-import{u as e,c as o,o as t,a}from"./Bj4-uZt8.js";const n=a("p",null,"Technology",-1),s=[n],u={__name:"index",setup(c){return e({title:"Qualiton | Technology"}),(l,_)=>(t(),o("div",null,s))}};export{u as default};
