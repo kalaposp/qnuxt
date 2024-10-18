2dd568b588f0ecf405b855fcb70b46bb4439baf3
kalaposp <peter.kalapos@audiohungary.hu>
2024-10-18 10:44:53 +0200

    commit-184 241018

--- build-assets/cSqrRMKF.js
+++
@@ -1,1 +0,0 @@
-import{u as t,c as a,a as o,o as s}from"./C15NjvMk.js";const c={__name:"index",setup(r){return t({title:"Legacy Products - Qualiton"}),(n,e)=>(s(),a("div",null,e[0]||(e[0]=[o("p",null,"Legacy Products",-1)])))}};export{c as default};
